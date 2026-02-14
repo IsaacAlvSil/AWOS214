daa1ae0a58ffac6be1a73185a8074b45eaf1db3d
isaac alvarez silva <123046942@upq.edu.mx>
2026-02-13 20:32:09 -0600

    Practica 3 - 2do avance

--- miApi/app/main.py
+++ miApi/app/main.py
@@ -1,5 +1,5 @@
 #Importaciones
-from fastapi import FastAPI
+from fastapi import FastAPI, status, HTTPException
 import asyncio
 from typing import Optional
 
@@ -31,12 +31,12 @@ async def hola():
             "estatus": "200"
             }
 
-@app.get("/v1/usuario/{id}", tags=["Parametro obligatorio"])
+@app.get("/v1/parametroOp/{id}", tags=["Parametro obligatorio"])
 async def consultaUno(id:int):
     return {"Se encontro usuario" : id}
 
 
-@app.get("/v1/usuarios/", tags=["Parametro opcional"])
+@app.get("/v1/parametroOp/", tags=["Parametro opcional"])
 async def consultaTodos(id:Optional[int]=None):
     if id is not None:
         for usuario in usuarios:
@@ -45,3 +45,112 @@ async def consultaTodos(id:Optional[int]=None):
         return{"mensaje:":"usuario no encontrado", "usuario":id}
     else:
         return{"mensaje:":"No se proporciono id"}
+    
+
+@app.get("/v1/usuarios/", tags=["CRUD HTTP"])
+async def leer_usuarios():
+    return{
+        "status":"200",
+        "total": len(usuarios),
+        "usuarios":usuarios
+    }
+
+@app.post("/v1/usuarios/",tags=["CRUD HTTP"],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr ["id"] == usuario.get("id"):
+            raise HTTPException(
+                status_code=400,
+                detail="El id ya existe"
+            )
+    usuarios.append(usuario)
+    return{
+        "mensaje":"Usuario agregado",
+        "Usuario":usuario
+    }
+    
+
+#Importaciones
+from fastapi import FastAPI, status, HTTPException
+import asyncio
+from typing import Optional
+
+#instancia del servidor 
+app = FastAPI(
+    title="Mi primer API",
+    description="Isaac Silva",
+    version="1.0.0"
+    )
+#BD ficticia
+usuarios=[
+    {"id": 1, "nombre":"Juan", "edad" :21 },
+    {"id": 2, "nombre":"Isra", "edad" :23 },
+    {"id": 3, "nombre":"Abdiel", "edad" :21 },
+    {"id": 4, "nombre":"Jafet", "edad" :24 },
+    {"id": 5, "nombre":"Roger", "edad" :19 },
+]
+
+#endpoints
+@app.get("/", tags=["Inicio"])
+async def bienvenida():
+    return {"mensaje" : "¡Bienvenido a mi API!"}
+
+@app.get("/HolaMundo" ,tags=["Bienvenida Asincrona"])
+async def hola():
+    await asyncio.sleep(4)
+    return {"mensaje" : "¡Hola mundo FastAPI! ",
+            "estatus": "200"
+            }
+
+@app.get("/v1/parametroOp/{id}", tags=["Parametro obligatorio"])
+async def consultaUno(id:int):
+    return {"Se encontro usuario" : id}
+
+
+@app.get("/v1/parametroOp/", tags=["Parametro opcional"])
+async def consultaTodos(id:Optional[int]=None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario["id"] == id:
+                return{"mensaje:":"usuario encontrado", "usuario":usuario}
+        return{"mensaje:":"usuario no encontrado", "usuario":id}
+    else:
+        return{"mensaje:":"No se proporciono id"}
+    
+
+@app.get("/v1/usuarios/", tags=["CRUD HTTP"])
+async def leer_usuarios():
+    return{
+        "status":"200",
+        "total": len(usuarios),
+        "usuarios":usuarios
+    }
+
+@app.post("/v1/usuarios/",tags=["CRUD HTTP"],status_code=status.HTTP_201_CREATED)
+async def crear_usuario(usuario:dict):
+    for usr in usuarios:
+        if usr ["id"] == usuario.get("id"):
+            raise HTTPException(
+                status_code=400,
+                detail="El id ya existe"
+            )
+    usuarios.append(usuario)
+    return{
+        "mensaje":"Usuario agregado",
+        "Usuario":usuario
+    }
+
+@app.put("/v1/usuarios/{id_buscado}", tags=["CRUD HTTP"])
+async def actualizar_usuario(id_buscado: int, datos_nuevos: dict):
+    for usr in usuarios:
+        if usr["id"] == id_buscado:
+            usr.update(datos_nuevos)
+            return {
+                "mensaje": "Usuario actualizado",
+                "usuario": usr
+            }
+        
+    raise HTTPException(
+        status_code=404,
+        detail="Usuario no encontrado"
+    )  
